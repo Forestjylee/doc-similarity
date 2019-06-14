63f5f52f22bf40c15ca94e865469869eabfbe49e
Junyi <36435335+Forest75@users.noreply.github.com>
2019-06-14 16:59:30 +0800

    Update encryption.py
    修复base64编码可能出现+或/符号，导致用户无法登陆的bug

--- SimilarityDjango/SimilarityApp/encryption.py
+++ SimilarityDjango/SimilarityApp/encryption.py
@@ -10,11 +10,15 @@ import base64# 使用base64简单加密
 
 # 加密
 def encrypt(source_string):
-    encrypted_string = base64.b64encode(source_string.encode('utf-8'))
+    encrypted_string = str(base64.b64encode(source_string.encode('utf-8')), 'utf-8')
+    n = encrypted_string.replace('/', '_')
+    n = n.replace('+', '.')
     return str(encrypted_string, 'utf-8')
 
 # 解密
 def decrypt(encrypted_string):
-    source_string = base64.b64decode(bytes(encrypted_string, encoding='utf-8'))
+    n = encrypted_string.replace('.', '+')
+    n = n.replace('_', '/')
+    source_string = base64.b64decode(bytes(n, encoding='utf-8'))
     return str(source_string, encoding='utf-8')
 
